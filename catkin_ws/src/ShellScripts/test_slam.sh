8beb2dc613b552c5c2ba43b1e4db229c10f76e36
Brad Huang <brad820309@gmail.com>
2018-08-11 16:12:53 +0000

    Add world file reference in test_slam.sh

--- catkin_ws/src/ShellScripts/test_slam.sh
+++ catkin_ws/src/ShellScripts/test_slam.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+export TURTLEBOT_3D_SENSOR=kinect
+export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/World/TestRoom.world
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm -e "rosrun gmapping slam_gmapping" &
