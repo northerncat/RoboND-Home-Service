1cfd865c3b138f49d06071b918c3c2c28e2e9915
Brad Huang <brad820309@gmail.com>
2018-08-14 02:39:26 +0000

    Removing hardcoded paths

--- catkin_ws/src/ShellScripts/test_navigation.sh
+++ catkin_ws/src/ShellScripts/test_navigation.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 export TURTLEBOT_3D_SENSOR=kinect
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/World/TestRoom.world
-export TURTLEBOT_GAZEBO_MAP_FILE=/home/workspace/catkin_ws/src/World/myMap.yaml
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
--- catkin_ws/src/ShellScripts/wall_follower.sh
+++ catkin_ws/src/ShellScripts/wall_follower.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 export TURTLEBOT_3D_SENSOR=kinect
-export TURTLEBOT_GAZEBO_WORLD_FILE=/home/workspace/catkin_ws/src/World/TestRoom.world
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm -e "roslaunch custom_gmapping_demo.launch" &
